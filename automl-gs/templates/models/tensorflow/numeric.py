cdb2022cad22ac5e2a4c9b58a442ac69d6377826
Max Woolf <max@minimaxir.com>
2019-02-25 21:55:33 -0800

    Add more numeric model options

--- automl-gs/templates/models/tensorflow/numeric.py
+++ automl-gs/templates/models/tensorflow/numeric.py
@@ -1,1 +1,7 @@
+    {% if params['numeric_strat'] in ['minmax', 'standard'] %}
     input_{{ field }} = Input(shape=(1,), name='input_{{ field }}')
+    {% elif params['numeric_strat'] == 'quantiles' %}
+    input_{{ field }} = Input(shape=(4,), name='input_{{ field }}')
+    {% else %}
+    input_{{ field }} = Input(shape=(10,), name='input_{{ field }}')
+    {% endif %}
