1dcb9e25cb53a83583172f61d7454b057c4c7ff7
Max Woolf <max@minimaxir.com>
2019-03-22 20:38:33 -0700

    Handle special case for tensorflow cat input

--- automl-gs/templates/models/tensorflow/categorical.py
+++ automl-gs/templates/models/tensorflow/categorical.py
@@ -1,3 +1,4 @@
     # {{ field_raw }}
-    input_{{ field }} = Input(shape=(len(encoders['{{ field }}_encoder'].classes_),), name="input_{{ field }}")
+    input_{{ field }}_size = len(encoders['{{ field }}_encoder'].classes_)
+    input_{{ field }} = Input(shape=(input_{{ field }}_size if input_{{ field }}_size != 2 else 1,), name="input_{{ field }}")
 
