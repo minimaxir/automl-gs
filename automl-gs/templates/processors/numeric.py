9c10e31e3069526f9b46eb1c54991ad3b07576f0
Max Woolf <max@minimaxir.com>
2019-03-23 17:20:49 -0700

    Make numeric processor syntax uniform with others

--- automl-gs/templates/processors/numeric.py
+++ automl-gs/templates/processors/numeric.py
@@ -1,6 +1,7 @@
     # {{ field_raw }}
     {% if params['numeric_strat'] in ['minmax', 'standard'] %}
-    {{ field }}_enc = encoders['{{ field }}_encoder'].transform(df['{{ field_raw }}'].values.reshape(-1, 1))
+    {{ field }}_enc = df['{{ field_raw }}'].values.reshape(-1, 1)
+    {{ field }}_enc = encoders['{{ field }}_encoder'].transform({{ field }}_enc)
     {% endif %}
     {% if params['numeric_strat'] in ['quantiles', 'percentiles'] %}
     {{ field }}_enc = pd.cut(df['{{ field_raw }}'].values, encoders['{{ field }}_bins'], labels=False, include_lowest=True)
